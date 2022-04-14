0158caa6dacb043ea6d8b6ebcfa5caa0fa4f4a23
LuciSirr <67655837+LuciSirr@users.noreply.github.com>
2022-04-14 21:50:44 +0200

    Add files via upload

---
+++ src/setup.py
@@ -0,0 +1,12 @@
+from setuptools import find_packages, setup
+setup(
+    name='ivs_project_2',
+    packages=find_packages(include=['math_lib']),
+    version='0.1.0',
+    description='Math library for calculator',
+    author='Lucia Šírová',
+    install_requires=[],
+    setup_requires=['pytest-runner'],
+    tests_require=['pytest==4.4.1'],
+    test_suite='tests',
+)
