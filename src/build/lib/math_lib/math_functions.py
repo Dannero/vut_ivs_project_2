f108b9020de8a9873e9159e60c05d2b298e671b7
LuciSirr <67655837+LuciSirr@users.noreply.github.com>
2022-04-14 21:36:09 +0200

    Add files via upload

---
+++ src/build/lib/math_lib/math_functions.py
@@ -0,0 +1,24 @@
+def plus(a: float, b: float) -> float:
+    return(a + b)
+
+def minus(a: float, b: float) -> float:
+    return(a - b)
+
+def multiply(a: float, b: float) -> float:
+    return(a * b)
+
+def divide(a: float, b: float) -> float:
+    return(a / b)
+
+
+def factorial(a: float) -> float:
+    return(a)
+
+def power(a: float, exponent: int) -> float:
+    return(a)
+
+def square(a: float, root: int) -> float:
+    return(a)
+
+def nieco_random_co_si_vymyslime_uwu(a: float) -> float:
+    return(a)
