161735c35c59b35cc17a33626e6c71654f4ca81e
Dannero <denny.blasko@gmail.com>
2022-04-27 17:46:08 +0200

    GUI and Profiling Doxygen format comments added

--- src/gui.py
+++ src/gui.py
@@ -1,14 +1,12 @@
 ##
-# @mainpage xsirov01_ivs Calculator  
-# @brief GUI of the calculator implementing mathematical functions
-#
+# @mainpage xsirov01_ivs Calculator 
 # 
+# @brief IVS Project 2 Calculator Implementation
 
 ##
 # @file gui.py  
-# @brief GUI of the calculator implementing mathematical functions
-#
 # 
+# @brief GUI of the calculator implementing mathematical functions
 from math_lib import math_functions 
 from tkinter import *
 from idlelib.tooltip import Hovertip
@@ -46,6 +44,11 @@ def display_remove():
         expression_eval = expression_eval[:-1]
     calculator_text.set(expression)
 
+##
+# @brief Display text
+# 
+# Updates expression after action and displays new expression on the display
+
 def display_text(item):
     global expression
     global expression_eval
@@ -60,37 +63,36 @@ def display_text(item):
 # @brief Evaluate
 # Evaluates the value of the expression
 #
-#
-#
+# Arranges the expression into an array of values and operations, then computes the values of operations with higher precedence before evaluating the whole expression
 def evaluate():
         global expression_eval
         global expression
         character_array = []
         number = ""
-        #parses expression into array
+        ##parses expression into array
         for character in range(len(expression_eval)):
-            #checks if character is a number or .
-            #or if first character is - to make nagative number
+            ##checks if character is a number or .
+            ##or if first character is - to make nagative number
             if (ord(expression_eval[character]) >= 48 and ord(expression_eval[character]) <= 57) or ord(expression_eval[character]) == ord('.') or (ord(expression_eval[character]) == ord('-') and character == 0):
                 number = number + expression_eval[character]
-                #evaluates last number at the end of the expression
+                ##evaluates last number at the end of the expression
                 if (character == (len(expression_eval)-1)):
                     number = float(number)
                     if (number.is_integer()):
                         number = int(number)
                     character_array.append(number)
                     number=""
-            #checks if - is in bracket to make a negative number
+            ##checks if - is in bracket to make a negative number
             elif ord(expression_eval[character]) == ord('-') and ord(expression_eval[character-1]) == ord('('):
                 number = number + expression_eval[character]
-            #when character is not a number
+            ##when character is not a number
             elif ((ord(expression_eval[character]) <= 48 or ord(expression_eval[character]) >= 57) and ord(expression_eval[character]) != ord('.') and ord(expression_eval[character]) != ord('(')):
                 if number != "":
-                    #evaluates and appends number to array
+                    ##evaluates and appends number to array
                     number = float(number)
                     if (number.is_integer()):
                         number = int(number)
-                    #appends operand to array
+                    ##appends operand to array
                     character_array.append(number)
                     if ord(expression_eval[character]) != ord(')') :
                         character_array.append(expression_eval[character])
@@ -98,12 +100,17 @@ def evaluate():
                 else:
                     character_array.append(expression_eval[character])
             
+        ##
+        # @brief Operator precedence function
+        #
+        # Evaluates operations in three stages:
+        # 1. High precedence operations (power, root, natural log, factorial)
+        # 2. Mid precedence operations (multiply, divide)
+        # 3. Low precedence operations (plus, minus)
+        # 
+        # After evaluating an operation value, the value is imported to the expression array and the operation char and its input values are removed
 
-        #Operator precedence function
-        #Evaluates operations with higher operator precedence and replaces them with their results
-        #Removes the rest of the operations and operands from expression
-
-        #Operations with highest precedence (power, root, factorial, natural log)
+        ##Operations with highest precedence (power, root, factorial, natural log)
         i=0 
         result = ""
         while i < len(character_array):
@@ -124,8 +131,6 @@ def evaluate():
                 del character_array[i]
 
             elif (character_array[i] == "l"):
-                #if character_array[i+2] != ")":
-                    #todo syntax error
                 try:
                     character_array[i] = math_functions.naturallog(character_array[i+1])
                     del character_array[i+1]
@@ -148,7 +153,7 @@ def evaluate():
                 del character_array[i]
             else: i+=1
 
-        #Operations with mid precedence (multiply, divide)
+        ##Operations with mid precedence (multiply, divide)
         i = 0
         while i < len(character_array):
             if (character_array[i] == "*"):
@@ -167,16 +172,16 @@ def evaluate():
 
 
 
-        #Basic mathematic functions (plus, minus)
-        #calculates result
+        ##Basic mathematic functions (plus, minus)
+        ##calculates result
         if result != "":
             calculator_text.set(result)
         else:
             result = character_array[0]
 
-            #iters by 2
+            ##iteratess by 2
             for character in range(1,len(character_array),2):
-                #creates a pair of operator and operad to use in basic math functions 
+                ##creates a pair of operator and operad to use in basic math functions 
                 operator = character_array[character]
                 operand = character_array[character+1]
 
@@ -185,7 +190,7 @@ def evaluate():
                 elif (operator == "-"):
                     result = math_functions.minus(result,operand)
 
-        #displays result
+        ##displays result
             result = str(result)
             calculator_text.set(result)
         expression_eval=""
@@ -195,8 +200,12 @@ def evaluate():
         result=0
 
         
-    
-#reads keyboard input  
+##
+# @brief key 
+# @param key Key presed on the keyboard
+# @return Displays the char of corresponding key on display
+
+##reads keyboard input  
 def key(event):
     for k in range (10):
         if (event.char == str(k)):
@@ -230,7 +239,6 @@ def key(event):
 #
 # User can input expression by either clicking the buttons on the calculator or using corresponding keys on a keyboard
 # Hints are shown after hovering over a button
-#
 
 gui.configure(bg="#1A1D23")
 gui.geometry("312x413")  
@@ -257,7 +265,7 @@ buttons_frame = Frame(gui, width=312, height=272.5, bg="#1A1D23")
 
 
 ##first row
-log = Button(buttons_frame, text = "ln()", fg = "white", width = 10, height = 3, bd = 0, bg = "#5D70FD", cursor = "hand2", command= lambda: display_text("ln"))
+log = Button(buttons_frame, text = "ln", fg = "white", width = 10, height = 3, bd = 0, bg = "#5D70FD", cursor = "hand2", command= lambda: display_text("ln"))
 log.grid(row = 0, column = 0, padx = 1, pady = 1)
 log_tip = Hovertip(log,'This button enters a natural logarithm function, enter a number after this to get its natural logarithm')
 
--- src/profiling.py
+++ src/profiling.py
@@ -1,15 +1,25 @@
-# Standard Deviation Calculator
+##
+# @file profiling.py
+#
+# @brief Standard Deviation Calculator
+
 from math_lib import math_functions
 
 int_lst = list(map(float, input().split()))
 
-#calculates the mean value (sum of input values divided by the count of the input values)
+##
+# @brief Mean Evaluation
+#
+# Calculates the mean value (sum of input values divided by the count of the input values)
 mean = float(0)
 for i in range(len(int_lst)):
     mean = math_functions.plus(mean, int_lst[i])   
 mean = math_functions.divide(mean, len(int_lst)) 
 
-#calculates the sample value( sqrt( (1/n-1)*(sum(val^2) - n*mean^2 ) ) )
+##
+# @bried Sample evaluation
+#
+# Calculates the sample value( sqrt( (1/n-1)*(sum(val^2) - n*mean^2 ) ) )
 sample = float(0)
 for i in range(len(int_lst)):
     sample = math_functions.plus(sample, math_functions.power(int_lst[i],2))
